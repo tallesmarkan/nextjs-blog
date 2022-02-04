daa16383fb3f0abf5cc27c08c4c20a49a009b805
tallesmarkan <tallesmarkan@gmail.com>
2022-02-04 14:30:04 -0300

    update adjust

--- pages/index.js
+++ pages/index.js
@@ -10,40 +10,29 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="">Pics!</a>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <p className="description">public repo images</p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className="card">
+            <img src="./images/pic3.jpg"></img>
           </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="" className="card">
+            <img src="./images/pic7.jpg"></img>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className="card">
+            <img src="./images/pic14.jpg"></img>
           </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="" className="card">
+            <img src="./images/pic15.jpg"></img>
+          </a>
+          <a href="" className="card">
+            <img src="./images/pic16.jpg"></img>
+          </a>
+          <a href="" className="card">
+            <img src="./images/pic22.jpg"></img>
           </a>
         </div>
       </main>
